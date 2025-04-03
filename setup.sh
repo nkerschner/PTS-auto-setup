59879164c8a0428d7f144c65874240e69045b398
nkerschner <natekerschner@revivn.com>
2025-04-03 14:47:29 -0700

    Update setup.sh
    Updated to install fork of osx-cpu-temp that provides more accurate temperatures.

--- setup.sh
+++ setup.sh
@@ -12,8 +12,15 @@ echo 'eval "$(/usr/local/bin/brew shellenv)"' >> /Users/open/.zprofile
 eval "$(/usr/local/bin/brew shellenv)"
 
 
-brew install php stats osx-cpu-temp
+brew install php stats
 brew services restart php
+
+git clone https://github.com/BourgonLaurent/osx-cpu-temp
+cd osx-cpu-temp
+make
+sudo make install
+
+cd ~/
 
 git clone https://github.com/phoronix-test-suite/phoronix-test-suite
 cd ~/phoronix-test-suite
